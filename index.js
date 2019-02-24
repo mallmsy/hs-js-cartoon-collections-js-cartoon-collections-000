d8fde466999a2c6e59ab2d606a417a55df206daf
Mallory Woods <malvwoods@gmail.com>
2019-02-24 21:52:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function dwarfRollCall(dwarves) {
+   var rollCall = ''
+   for (let i = 0; i < dwarves.length; i++) {
+      var placeInLine = i + 1 + '. '
+      var fullName = placeInLine + dwarves[i] + ' '
+    rollCall = rollCall + fullName
+  }
+    return(rollCall)
+  }
+
+
+function summonCaptainPlanet(planeteerCalls) {
+  var shoutCalls = []
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    shoutCalls.push(`${planeteerCalls[i]}`.toUpperCase() + "!")
+  }
+  return(shoutCalls) 
 }
 
-function summonCaptainPlanet(planeteerCalls){
-}
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return(true)
+    } else {
+      return(false)
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  var finalResult = ''
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+      return(foods[i])
+    } 
+    else {
+      finalResult = "no cheese!"
+    }
+  }
+  return (finalResult)
 }
